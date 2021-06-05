16943db7ab0c8984f9a20a9df327744d7efe49ff
JaguarJack <82664165@qq.com>
2021-06-05 20:48:25 +0800

    update

--- src/Block/Elseif_.php
+++ src/Block/Elseif_.php
@@ -10,15 +10,23 @@ class Elseif_
      *
      * @time 2021年06月04日
      * @param $condition
-     * @return Elseif_|\PhpParser\Node\Stmt\ElseIf_
+     * @return Elseif_
      */
-    public static function condition($condition)
+    public static function condition($condition): Elseif_
     {
         self::$elseif = new \PhpParser\Node\Stmt\ElseIf_($condition);
 
         return new self;
     }
 
+    /**
+     * else if
+     *
+     * @author CatchAdmin
+     * @time 2021年06月05日
+     * @param array $stmt
+     * @return mixed
+     */
     public function block(array $stmt)
     {
         self::$elseif->stmts = $stmt;
